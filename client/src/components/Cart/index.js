3e2267e6e569154c46452a2123b1fcc9cd10ddf2
Aisha Osman <aisha.osman09@gmail.com>
2022-01-22 15:16:18 -0600

    re-added client

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import CartItem from '../CartItem';
-import Auth from '../../utils/auth';
-import './style.css';
-import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART } from '../../utils/actions';
+import React, { useEffect } from "react";
+import { idbPromise } from "../../utils/helpers"
+import CartItem from "../CartItem";
+import Auth from "../../utils/auth";
+import { useStoreContext } from "../../utils/GlobalState";
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import "./style.css";
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
 
+  useEffect(() => {
+    async function getCart() {
+      const cart = await idbPromise('cart', 'get');
+      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+    };
+
+    if (!state.cart.length) {
+      getCart();
+    }
+  }, [state.cart.length, dispatch]);
+
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
   }
+
   function calculateTotal() {
     let sum = 0;
     state.cart.forEach(item => {
@@ -28,36 +41,39 @@ const Cart = () => {
       </div>
     );
   }
+
   return (
-<div className="cart">
-  <div className="close" onClick={toggleCart}>[close]</div>
-  <h2>Shopping Cart</h2>
-  {state.cart.length ? (
-    <div>
-      {state.cart.map(item => (
-        <CartItem key={item._id} item={item} />
-      ))}
-      <div className="flex-row space-between">
-        <strong>Total: ${calculateTotal()}</strong>
-        {
-          Auth.loggedIn() ?
-            <button>
-              Checkout
-            </button>
-            :
-            <span>(log in to check out)</span>
-        }
-      </div>
+    <div className="cart">
+      <div className="close" onClick={toggleCart}>[close]</div>
+      <h2>Shopping Cart</h2>
+      {state.cart.length ? (
+        <div>
+          {state.cart.map(item => (
+            <CartItem key={item._id} item={item} />
+          ))}
+
+          <div className="flex-row space-between">
+            <strong>Total: ${calculateTotal()}</strong>
+
+            {
+              Auth.loggedIn() ?
+                <button>
+                  Checkout
+              </button>
+                :
+                <span>(log in to check out)</span>
+            }
+          </div>
+        </div>
+      ) : (
+          <h3>
+            <span role="img" aria-label="shocked">
+              😱
+          </span>
+          You haven't added anything to your cart yet!
+          </h3>
+        )}
     </div>
-  ) : (
-    <h3>
-      <span role="img" aria-label="shocked">
-        😱
-      </span>
-      You haven't added anything to your cart yet!
-    </h3>
-  )}
-</div>
   );
 };
 
--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,33 +1,41 @@
 import React from 'react';
-import { useStoreContext } from '../../utils/GlobalState';
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { useStoreContext } from "../../utils/GlobalState";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
+
   const [, dispatch] = useStoreContext();
 
-const removeFromCart = item => {
-  dispatch({
-    type: REMOVE_FROM_CART,
-    _id: item._id
-  });
-};
-
-const onChange = (e) => {
-  const value = e.target.value;
-
-  if (value === '0') {
+  const removeFromCart = item => {
     dispatch({
       type: REMOVE_FROM_CART,
       _id: item._id
     });
-  } else {
-    dispatch({
-      type: UPDATE_CART_QUANTITY,
-      _id: item._id,
-      purchaseQuantity: parseInt(value)
-    });
+    idbPromise('cart', 'delete', { ...item });
+
+  };
+
+  const onChange = (e) => {
+    const value = e.target.value;
+    if (value === '0') {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id
+      });
+      idbPromise('cart', 'delete', { ...item });
+
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value)
+      });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+
+    }
   }
-};
+
   return (
     <div className="flex-row">
       <div>
@@ -41,17 +49,18 @@ const onChange = (e) => {
         <div>
           <span>Qty:</span>
           <input
-          type="number"
-          placeholder="1"
-          value={item.purchaseQuantity}
-          onChange={onChange}
-        />
-        <span
-          role="img"
-          aria-label="trash"
-          onClick={() => removeFromCart(item)}>
-          🗑️
-        </span>
+            type="number"
+            placeholder="1"
+            value={item.purchaseQuantity}
+            onChange={onChange}
+          />
+          <span
+            role="img"
+            aria-label="trash"
+            onClick={() => removeFromCart(item)}
+          >
+            🗑️
+          </span>
         </div>
       </div>
     </div>
